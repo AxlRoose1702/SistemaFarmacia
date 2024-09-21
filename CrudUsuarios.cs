255135fae72fefe1ecffc358618544bfb2046b8b
Axel José Mosquito <112713383+AxlRoose1702@users.noreply.github.com>
2024-09-21 11:04:24 -0600

    Comentario de prueba

--- CrudUsuarios.cs
+++ CrudUsuarios.cs
@@ -12,6 +12,7 @@ namespace SistemaFarmacia
         {
             InitializeComponent();
             conn = new SqlConnection("Data Source=LAPTOP-JC6HE824;Initial Catalog=Db_farmacia;Integrated Security=True;");
+            //Prueba
         }
 
         private void label1_Click(object sender, EventArgs e)
