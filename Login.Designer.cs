cae4d9dec202d5c2b7f498e7c8261d995d43e323
Axel José Mosquito <112713383+AxlRoose1702@users.noreply.github.com>
2024-09-21 21:24:19 -0600

    Se agrega el login, y validacion con la base de datos.

---
+++ Login.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace SistemaFarmacia
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPasswordUser = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(147, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Iniciar Sesion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(148, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingresar Usuario";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(153, 116);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(185, 22);
+            this.txtUser.TabIndex = 2;
+            // 
+            // txtPasswordUser
+            // 
+            this.txtPasswordUser.Location = new System.Drawing.Point(153, 221);
+            this.txtPasswordUser.Name = "txtPasswordUser";
+            this.txtPasswordUser.Size = new System.Drawing.Size(185, 22);
+            this.txtPasswordUser.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(127, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(230, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ingresar Contraseña";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnIngresar);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtPasswordUser);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtUser);
+            this.groupBox1.Location = new System.Drawing.Point(142, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(487, 360);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar.Location = new System.Drawing.Point(186, 270);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(115, 38);
+            this.btnIngresar.TabIndex = 5;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPasswordUser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnIngresar;
+    }
+}
---
+++ Login.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SistemaFarmacia
+{
+    public partial class Login : Form
+    {
+        private SqlConnection conn;
+        public Login()
+        {
+            InitializeComponent();
+            conn = new SqlConnection("Data Source=LAPTOP-JC6HE824;Initial Catalog=Db_farmacia;Integrated Security=True;");
+
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            string user, password;
+
+            user = txtUser.Text.Trim();
+            password = txtPasswordUser.Text.Trim();
+
+            conn.Open();
+            string QryConsultarUsuarios = "Select Usuario, Contrasena from tbl_usuarios where Usuario = @user";
+            using (SqlCommand cmd = new SqlCommand(QryConsultarUsuarios, conn))
+            {
+                cmd.Parameters.AddWithValue("@user", user);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        string dbPasswordHash = reader["Contrasena"].ToString();
+                        MessageBox.Show("Sesion iniciada con exito!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuario no enontrado.");
+                    }
+                }
+
+            }
+            conn.Close();
+        }
+    }
+}
