69d2f521d08622cf8e7d142e8706df4df004c195
Axel José Mosquito <112713383+AxlRoose1702@users.noreply.github.com>
2024-09-24 20:55:39 -0600

    se agrega pantalla de crud de dealle de venta.

---
+++ CrudDetalleVentas.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace SistemaFarmacia
+{
+    partial class CrudDetalleVentas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboPrecio = new System.Windows.Forms.ComboBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCodigoMedicamento = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCodigoVenta = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCodigoDVenta = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtBusqueda = new System.Windows.Forms.TextBox();
+            this.dgviewDetalleVenta = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgviewDetalleVenta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.comboPrecio);
+            this.groupBox1.Controls.Add(this.txtCantidad);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtCodigoMedicamento);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtCodigoVenta);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtCodigoDVenta);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(45, 79);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(421, 514);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 314);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(201, 32);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Precio Unitario";
+            // 
+            // comboPrecio
+            // 
+            this.comboPrecio.FormattingEnabled = true;
+            this.comboPrecio.Items.AddRange(new object[] {
+            "1.50",
+            "2.00",
+            "5.00",
+            "0.50",
+            "1.20",
+            "10.00",
+            "1.00",
+            "15.00",
+            "8.50",
+            "6.00"});
+            this.comboPrecio.Location = new System.Drawing.Point(12, 349);
+            this.comboPrecio.Name = "comboPrecio";
+            this.comboPrecio.Size = new System.Drawing.Size(286, 24);
+            this.comboPrecio.TabIndex = 21;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.BackColor = System.Drawing.SystemColors.Window;
+            this.txtCantidad.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCantidad.Location = new System.Drawing.Point(12, 275);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(280, 22);
+            this.txtCantidad.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 240);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(129, 32);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Cantidad";
+            // 
+            // txtCodigoMedicamento
+            // 
+            this.txtCodigoMedicamento.BackColor = System.Drawing.SystemColors.Window;
+            this.txtCodigoMedicamento.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCodigoMedicamento.Location = new System.Drawing.Point(12, 205);
+            this.txtCodigoMedicamento.Name = "txtCodigoMedicamento";
+            this.txtCodigoMedicamento.Size = new System.Drawing.Size(280, 22);
+            this.txtCodigoMedicamento.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(283, 32);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Codigo Medicamento";
+            // 
+            // txtCodigoVenta
+            // 
+            this.txtCodigoVenta.BackColor = System.Drawing.SystemColors.Window;
+            this.txtCodigoVenta.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCodigoVenta.Location = new System.Drawing.Point(12, 130);
+            this.txtCodigoVenta.Name = "txtCodigoVenta";
+            this.txtCodigoVenta.Size = new System.Drawing.Size(280, 22);
+            this.txtCodigoVenta.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(187, 32);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Codigo Venta";
+            // 
+            // txtCodigoDVenta
+            // 
+            this.txtCodigoDVenta.BackColor = System.Drawing.SystemColors.Window;
+            this.txtCodigoDVenta.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCodigoDVenta.Location = new System.Drawing.Point(12, 60);
+            this.txtCodigoDVenta.Name = "txtCodigoDVenta";
+            this.txtCodigoDVenta.Size = new System.Drawing.Size(280, 22);
+            this.txtCodigoDVenta.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(323, 32);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Codigo Detalle de Venta";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(373, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(391, 32);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "DATOS DETALLE DE VENTA";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(712, 513);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(114, 34);
+            this.btnEliminar.TabIndex = 35;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Location = new System.Drawing.Point(592, 513);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(114, 34);
+            this.btnEditar.TabIndex = 34;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(472, 513);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(114, 34);
+            this.btnGuardar.TabIndex = 33;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(961, 513);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(114, 34);
+            this.btnCancelar.TabIndex = 32;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(712, 53);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(114, 34);
+            this.btnBuscar.TabIndex = 31;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // txtBusqueda
+            // 
+            this.txtBusqueda.BackColor = System.Drawing.SystemColors.Window;
+            this.txtBusqueda.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBusqueda.Location = new System.Drawing.Point(845, 61);
+            this.txtBusqueda.Name = "txtBusqueda";
+            this.txtBusqueda.Size = new System.Drawing.Size(280, 22);
+            this.txtBusqueda.TabIndex = 30;
+            // 
+            // dgviewDetalleVenta
+            // 
+            this.dgviewDetalleVenta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgviewDetalleVenta.Location = new System.Drawing.Point(472, 104);
+            this.dgviewDetalleVenta.Name = "dgviewDetalleVenta";
+            this.dgviewDetalleVenta.RowHeadersWidth = 51;
+            this.dgviewDetalleVenta.RowTemplate.Height = 24;
+            this.dgviewDetalleVenta.Size = new System.Drawing.Size(766, 391);
+            this.dgviewDetalleVenta.TabIndex = 29;
+            // 
+            // CrudDetalleVentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1283, 714);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBusqueda);
+            this.Controls.Add(this.dgviewDetalleVenta);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "CrudDetalleVentas";
+            this.Text = "CrudDetalleVentas";
+            this.Load += new System.EventHandler(this.CrudDetalleVentas_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgviewDetalleVenta)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboPrecio;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCodigoMedicamento;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCodigoVenta;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCodigoDVenta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtBusqueda;
+        private System.Windows.Forms.DataGridView dgviewDetalleVenta;
+    }
+}
---
+++ CrudDetalleVentas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SistemaFarmacia
+{
+    public partial class CrudDetalleVentas : Form
+    {
+        private SqlConnection conn;
+        public CrudDetalleVentas()
+        {
+            InitializeComponent();
+            conn = new SqlConnection("Data Source=LAPTOP-JC6HE824;Initial Catalog=Db_farmacia;Integrated Security=True;");
+        }
+
+        private void CrudDetalleVentas_Load(object sender, EventArgs e)
+        {
+            string QryConsultarUsuarios = "Select * from tbl_detalle_ventas";
+            SqlDataAdapter adapter = new SqlDataAdapter(QryConsultarUsuarios, conn);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dgviewDetalleVenta.DataSource = dt;
+
+        }
+    }
+}
