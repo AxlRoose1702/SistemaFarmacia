d2537a89dbc4ef13b999bb46ff8fa4e58a9899d8
God-Lech <89615777+God-Lech@users.noreply.github.com>
2024-10-05 09:56:04 -0600

    Crud de Ventas

---
+++ CrudVentas.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace SistemaFarmacia
+{
+    partial class CrudVentas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboPAGO = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCodigoU = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.date = new System.Windows.Forms.DateTimePicker();
+            this.txtCodigoC = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCodigoV = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtBusqueda = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgviewVentas = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgviewVentas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.comboPAGO);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtCodigoU);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.date);
+            this.groupBox1.Controls.Add(this.txtCodigoC);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtCodigoV);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(40, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(421, 623);
+            this.groupBox1.TabIndex = 46;
+            this.groupBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(6, 520);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(93, 32);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Fecha";
+            // 
+            // comboPAGO
+            // 
+            this.comboPAGO.FormattingEnabled = true;
+            this.comboPAGO.Items.AddRange(new object[] {
+            "Efectivo",
+            "Tarjeta",
+            "Transferencia"});
+            this.comboPAGO.Location = new System.Drawing.Point(6, 368);
+            this.comboPAGO.Name = "comboPAGO";
+            this.comboPAGO.Size = new System.Drawing.Size(286, 24);
+            this.comboPAGO.TabIndex = 21;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 309);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(183, 32);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Tipo de Pago";
+            // 
+            // txtCodigoU
+            // 
+            this.txtCodigoU.BackColor = System.Drawing.SystemColors.Window;
+            this.txtCodigoU.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCodigoU.Location = new System.Drawing.Point(12, 260);
+            this.txtCodigoU.Name = "txtCodigoU";
+            this.txtCodigoU.Size = new System.Drawing.Size(280, 22);
+            this.txtCodigoU.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(210, 32);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Código Usuario";
+            // 
+            // date
+            // 
+            this.date.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.date.Location = new System.Drawing.Point(6, 580);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(286, 22);
+            this.date.TabIndex = 13;
+            // 
+            // txtCodigoC
+            // 
+            this.txtCodigoC.AcceptsReturn = true;
+            this.txtCodigoC.BackColor = System.Drawing.SystemColors.Window;
+            this.txtCodigoC.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCodigoC.Location = new System.Drawing.Point(12, 164);
+            this.txtCodigoC.Name = "txtCodigoC";
+            this.txtCodigoC.Size = new System.Drawing.Size(280, 22);
+            this.txtCodigoC.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(202, 32);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Código Cliente";
+            // 
+            // txtCodigoV
+            // 
+            this.txtCodigoV.BackColor = System.Drawing.SystemColors.Window;
+            this.txtCodigoV.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCodigoV.Location = new System.Drawing.Point(12, 72);
+            this.txtCodigoV.Name = "txtCodigoV";
+            this.txtCodigoV.Size = new System.Drawing.Size(280, 22);
+            this.txtCodigoV.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(226, 32);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Código de Venta";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(797, 564);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(114, 34);
+            this.btnEliminar.TabIndex = 45;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Location = new System.Drawing.Point(677, 564);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(114, 34);
+            this.btnEditar.TabIndex = 44;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(557, 564);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(114, 34);
+            this.btnGuardar.TabIndex = 43;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(1046, 564);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(114, 34);
+            this.btnCancelar.TabIndex = 42;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(797, 104);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(114, 34);
+            this.btnBuscar.TabIndex = 41;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // txtBusqueda
+            // 
+            this.txtBusqueda.BackColor = System.Drawing.SystemColors.Window;
+            this.txtBusqueda.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBusqueda.Location = new System.Drawing.Point(930, 112);
+            this.txtBusqueda.Name = "txtBusqueda";
+            this.txtBusqueda.Size = new System.Drawing.Size(280, 22);
+            this.txtBusqueda.TabIndex = 40;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(533, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 32);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "DATOS VENTAS";
+            // 
+            // dgviewVentas
+            // 
+            this.dgviewVentas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgviewVentas.Location = new System.Drawing.Point(557, 155);
+            this.dgviewVentas.Name = "dgviewVentas";
+            this.dgviewVentas.RowHeadersWidth = 51;
+            this.dgviewVentas.RowTemplate.Height = 24;
+            this.dgviewVentas.Size = new System.Drawing.Size(766, 391);
+            this.dgviewVentas.TabIndex = 38;
+            // 
+            // CrudVentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1362, 664);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBusqueda);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgviewVentas);
+            this.Name = "CrudVentas";
+            this.Text = "CrudVentas";
+            this.Load += new System.EventHandler(this.CrudVentas_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgviewVentas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboPAGO;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCodigoU;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker date;
+        private System.Windows.Forms.TextBox txtCodigoC;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCodigoV;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtBusqueda;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgviewVentas;
+    }
+}
---
+++ CrudVentas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaFarmacia
+{
+    public partial class CrudVentas : Form
+    {
+        private SqlConnection conn;
+        public CrudVentas()
+        {
+            InitializeComponent();
+            //conn = new SqlConnection("Data Source=LAPTOP-JC6HE824;Initial Catalog=Db_farmacia;Integrated Security=True;");
+            conn = new SqlConnection("server=DESKTOP-QDTQ6AS\\SQLEXPRESS; database=Db_farmacia; integrated security=true");
+            conn = new SqlConnection("server=GODLECH\\SQLEXPRESS; database=Db_farmacia; integrated security=true");
+
+        }
+
+        private void CrudVentas_Load(object sender, EventArgs e)
+        {
+            string QryConsultarUsuarios = "Select * from tbl_ventas";
+            SqlDataAdapter adapter = new SqlDataAdapter(QryConsultarUsuarios, conn);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dgviewVentas.DataSource = dt;
+        }
+    }
+}
