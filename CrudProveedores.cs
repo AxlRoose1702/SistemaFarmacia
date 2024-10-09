fb0e3b9d7e4a3a4032b6d818c95d2fdf35072649
God-Lech <89615777+God-Lech@users.noreply.github.com>
2024-10-09 15:27:40 -0600

    Cambios esteticos en Crud Proveedores 9 de octubre

--- CrudProveedores.cs
+++ CrudProveedores.cs
@@ -10,9 +10,12 @@ namespace SistemaFarmacia
         private SqlConnection conn;
         public CrudProveedores()
         {
+            // CONEXIONES A BASE DE DATOS (3 VERSIONES)
             InitializeComponent();
-            conn = new SqlConnection("Data Source=LAPTOP-JC6HE824;Initial Catalog=Db_farmacia;Integrated Security=True;");
+            //conn = new SqlConnection("Data Source=LAPTOP-JC6HE824;Initial Catalog=Db_farmacia;Integrated Security=True;");
+            conn = new SqlConnection("Data Source=GODLECH\\SQLEXPRESS;Initial Catalog=Db_farmacia;Integrated Security=True;");
             //conn = new SqlConnection("server=DESKTOP-QDTQ6AS\\SQLEXPRESS; database=Db_farmacia; integrated security=true");
+        
         }
 
         private void CrudProveedores_Load(object sender, EventArgs e)
@@ -39,11 +42,40 @@ namespace SistemaFarmacia
             conn.Open();
             int cant = cmd.ExecuteNonQuery();
 
-
+            // VERIFICA SI SE AGREGARON FILAS
+            
             if (cant > 0)
             {
                 MessageBox.Show("Se ha insertado los datos correctamente", "¡Datos Guardados!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                //DEJAMOS EN BLANCO TODAS LAS CASILLAS
+                txtProveedor.Text = "";
+                txtNIT.Text = "";
+                txtTEL.Text = "";
+                txtEmail.Text = "";
+                txtDireccion.Text = "";
+                cboxEstado.Text = "";
+                dtpfecha.Text = "";
+                
+
+                // MODIFICAR SEGUN CRUD UTILIZADO
+                string QryConsultarProveedores = "Select * from tbl_proveedores";
+                SqlDataAdapter adapter = new SqlDataAdapter(QryConsultarProveedores, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dgviewProveedores.DataSource = dt;
+
             }
+            else
+            {
+                MessageBox.Show("No se agregó registro!!!", "Advertencia!!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            // Cerrar la conexión
+            conn.Close();
+
+
+
         }
     }
 }
