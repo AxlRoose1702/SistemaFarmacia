d5c0b9cb13cd5cb5ef45da5c882cbaa6811b4149
God-Lech <89615777+God-Lech@users.noreply.github.com>
2024-10-09 17:01:11 -0600

    Crud Ventas con boton guardar

--- CrudVentas.Designer.cs
+++ CrudVentas.Designer.cs
@@ -34,7 +34,7 @@
             this.label5 = new System.Windows.Forms.Label();
             this.txtCodigoU = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.date = new System.Windows.Forms.DateTimePicker();
+            this.dateFecha = new System.Windows.Forms.DateTimePicker();
             this.txtCodigoC = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.txtCodigoV = new System.Windows.Forms.TextBox();
@@ -46,9 +46,9 @@
             this.btnBuscar = new System.Windows.Forms.Button();
             this.txtBusqueda = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.dgviewVentas = new System.Windows.Forms.DataGridView();
+            this.dgvVentas = new System.Windows.Forms.DataGridView();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgviewVentas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVentas)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -58,14 +58,14 @@
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.txtCodigoU);
             this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.date);
+            this.groupBox1.Controls.Add(this.dateFecha);
             this.groupBox1.Controls.Add(this.txtCodigoC);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.txtCodigoV);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Location = new System.Drawing.Point(40, 21);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(421, 623);
+            this.groupBox1.Size = new System.Drawing.Size(421, 554);
             this.groupBox1.TabIndex = 46;
             this.groupBox1.TabStop = false;
             // 
@@ -73,7 +73,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(6, 520);
+            this.label7.Location = new System.Drawing.Point(6, 419);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(93, 32);
             this.label7.TabIndex = 22;
@@ -120,13 +120,13 @@
             this.label4.TabIndex = 17;
             this.label4.Text = "Código Usuario";
             // 
-            // date
+            // dateFecha
             // 
-            this.date.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.date.Location = new System.Drawing.Point(6, 580);
-            this.date.Name = "date";
-            this.date.Size = new System.Drawing.Size(286, 22);
-            this.date.TabIndex = 13;
+            this.dateFecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateFecha.Location = new System.Drawing.Point(6, 479);
+            this.dateFecha.Name = "dateFecha";
+            this.dateFecha.Size = new System.Drawing.Size(286, 22);
+            this.dateFecha.TabIndex = 13;
             // 
             // txtCodigoC
             // 
@@ -154,6 +154,7 @@
             this.txtCodigoV.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtCodigoV.Location = new System.Drawing.Point(12, 72);
             this.txtCodigoV.Name = "txtCodigoV";
+            this.txtCodigoV.ReadOnly = true;
             this.txtCodigoV.Size = new System.Drawing.Size(280, 22);
             this.txtCodigoV.TabIndex = 14;
             // 
@@ -170,7 +171,7 @@
             // btnEliminar
             // 
             this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEliminar.Location = new System.Drawing.Point(797, 564);
+            this.btnEliminar.Location = new System.Drawing.Point(796, 541);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(114, 34);
             this.btnEliminar.TabIndex = 45;
@@ -180,7 +181,7 @@
             // btnEditar
             // 
             this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEditar.Location = new System.Drawing.Point(677, 564);
+            this.btnEditar.Location = new System.Drawing.Point(676, 541);
             this.btnEditar.Name = "btnEditar";
             this.btnEditar.Size = new System.Drawing.Size(114, 34);
             this.btnEditar.TabIndex = 44;
@@ -190,17 +191,18 @@
             // btnGuardar
             // 
             this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGuardar.Location = new System.Drawing.Point(557, 564);
+            this.btnGuardar.Location = new System.Drawing.Point(556, 541);
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Size = new System.Drawing.Size(114, 34);
             this.btnGuardar.TabIndex = 43;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
             // btnCancelar
             // 
             this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancelar.Location = new System.Drawing.Point(1046, 564);
+            this.btnCancelar.Location = new System.Drawing.Point(1045, 541);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(114, 34);
             this.btnCancelar.TabIndex = 42;
@@ -210,7 +212,7 @@
             // btnBuscar
             // 
             this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBuscar.Location = new System.Drawing.Point(797, 104);
+            this.btnBuscar.Location = new System.Drawing.Point(912, 85);
             this.btnBuscar.Name = "btnBuscar";
             this.btnBuscar.Size = new System.Drawing.Size(114, 34);
             this.btnBuscar.TabIndex = 41;
@@ -221,7 +223,7 @@
             // 
             this.txtBusqueda.BackColor = System.Drawing.SystemColors.Window;
             this.txtBusqueda.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtBusqueda.Location = new System.Drawing.Point(930, 112);
+            this.txtBusqueda.Location = new System.Drawing.Point(1045, 93);
             this.txtBusqueda.Name = "txtBusqueda";
             this.txtBusqueda.Size = new System.Drawing.Size(280, 22);
             this.txtBusqueda.TabIndex = 40;
@@ -232,19 +234,19 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(533, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(231, 32);
+            this.label1.Size = new System.Drawing.Size(127, 32);
             this.label1.TabIndex = 39;
-            this.label1.Text = "DATOS VENTAS";
+            this.label1.Text = "VENTAS";
             // 
-            // dgviewVentas
+            // dgvVentas
             // 
-            this.dgviewVentas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgviewVentas.Location = new System.Drawing.Point(557, 155);
-            this.dgviewVentas.Name = "dgviewVentas";
-            this.dgviewVentas.RowHeadersWidth = 51;
-            this.dgviewVentas.RowTemplate.Height = 24;
-            this.dgviewVentas.Size = new System.Drawing.Size(766, 391);
-            this.dgviewVentas.TabIndex = 38;
+            this.dgvVentas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVentas.Location = new System.Drawing.Point(557, 134);
+            this.dgvVentas.Name = "dgvVentas";
+            this.dgvVentas.RowHeadersWidth = 51;
+            this.dgvVentas.RowTemplate.Height = 24;
+            this.dgvVentas.Size = new System.Drawing.Size(766, 391);
+            this.dgvVentas.TabIndex = 38;
             // 
             // CrudVentas
             // 
@@ -259,13 +261,13 @@
             this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.txtBusqueda);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dgviewVentas);
+            this.Controls.Add(this.dgvVentas);
             this.Name = "CrudVentas";
             this.Text = "CrudVentas";
             this.Load += new System.EventHandler(this.CrudVentas_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgviewVentas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVentas)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -279,7 +281,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txtCodigoU;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.DateTimePicker date;
+        private System.Windows.Forms.DateTimePicker dateFecha;
         private System.Windows.Forms.TextBox txtCodigoC;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtCodigoV;
@@ -291,6 +293,6 @@
         private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.TextBox txtBusqueda;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dgviewVentas;
+        private System.Windows.Forms.DataGridView dgvVentas;
     }
 }
--- CrudVentas.cs
+++ CrudVentas.cs
@@ -16,10 +16,11 @@ namespace SistemaFarmacia
         private SqlConnection conn;
         public CrudVentas()
         {
+            // CONEXIONES A BASE DE DATOS (3 VERSIONES)
             InitializeComponent();
             //conn = new SqlConnection("Data Source=LAPTOP-JC6HE824;Initial Catalog=Db_farmacia;Integrated Security=True;");
-            conn = new SqlConnection("server=DESKTOP-QDTQ6AS\\SQLEXPRESS; database=Db_farmacia; integrated security=true");
-            conn = new SqlConnection("server=GODLECH\\SQLEXPRESS; database=Db_farmacia; integrated security=true");
+            conn = new SqlConnection("Data Source=GODLECH\\SQLEXPRESS;Initial Catalog=Db_farmacia;Integrated Security=True;");
+            //conn = new SqlConnection("server=DESKTOP-QDTQ6AS\\SQLEXPRESS; database=Db_farmacia; integrated security=true");
 
         }
 
@@ -29,7 +30,51 @@ namespace SistemaFarmacia
             SqlDataAdapter adapter = new SqlDataAdapter(QryConsultarUsuarios, conn);
             DataTable dt = new DataTable();
             adapter.Fill(dt);
-            dgviewVentas.DataSource = dt;
+            dgvVentas.DataSource = dt;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            //SE ABRE LA CONEXIÓN SQL
+            int cant = 0;
+            conn.Open();
+
+            //SE MODIFICA SEGUN DATOS DE LA BASE DE DATOS Y [DISEÑO]
+            string QryAgregarVentas = "Insert into tbl_ventas (FechaVenta, CodigoCliente, CodigoUsuario, TipoPago) values (@FechaVenta, @CodigoCliente, @CodigoUsuario, @TipoPago)";
+            SqlCommand cmd = new SqlCommand(QryAgregarVentas, conn);
+            cmd.Parameters.AddWithValue("@FechaVenta", DateTime.Parse(dateFecha.Text));
+            cmd.Parameters.AddWithValue("@CodigoCliente", txtCodigoC.Text);
+            cmd.Parameters.AddWithValue("@CodigoUsuario", txtCodigoU.Text);
+            cmd.Parameters.AddWithValue("@TipoPago", comboPAGO.Text);
+
+
+            // VERIFICA SI SE AGREGARON FILAS
+            cant = cmd.ExecuteNonQuery();
+            if (cant > 0)
+            {
+                MessageBox.Show("Se ha insertado los datos correctamente", "¡Datos Guardados!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                //DEJAMOS EN BLANCO TODAS LAS CASILLAS
+                dateFecha.Text = "";
+                txtCodigoC.Text = "";
+                txtCodigoU.Text = "";
+                comboPAGO.Text = "";
+
+                // MODIFICAR SEGUN CRUD UTILIZADO
+                string QryConsultarMedicamentos = "Select * from tbl_ventas";
+                SqlDataAdapter adapter = new SqlDataAdapter(QryConsultarMedicamentos, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dgvVentas.DataSource = dt;
+
+            }
+            else
+            {
+                MessageBox.Show("No se agregó registro!!!", "Advertencia!!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            // Cerrar la conexión
+            conn.Close();
         }
     }
 }
