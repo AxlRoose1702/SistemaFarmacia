5b77f43799e3702c9967006cdde13694c139697b
Axel José Mosquito <112713383+AxlRoose1702@users.noreply.github.com>
2024-10-19 10:07:47 -0600

    Se agrega funcionalidad para eliminar

--- CrudDetalleVentas.cs
+++ CrudDetalleVentas.cs
@@ -12,9 +12,9 @@ namespace SistemaFarmacia
         {
             // CONEXIONES A BASE DE DATOS (3 VERSIONES)
             InitializeComponent();
-            //conn = new SqlConnection("Data Source=LAPTOP-JC6HE824;Initial Catalog=Db_farmacia;Integrated Security=True;");
+            conn = new SqlConnection("Data Source=LAPTOP-JC6HE824;Initial Catalog=Db_farmacia;Integrated Security=True;");
             //conn = new SqlConnection("Data Source=GODLECH\\SQLEXPRESS;Initial Catalog=Db_farmacia;Integrated Security=True;");
-            conn = new SqlConnection("server=DESKTOP-QDTQ6AS\\SQLEXPRESS; database=Db_farmacia; integrated security=true");
+            //conn = new SqlConnection("server=DESKTOP-QDTQ6AS\\SQLEXPRESS; database=Db_farmacia; integrated security=true");
 
         }
 
@@ -30,7 +30,7 @@ namespace SistemaFarmacia
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(txtCodMedicamento.Text) || string.IsNullOrEmpty(txtCodVenta.Text) || string.IsNullOrEmpty(txtDescuento.Text) || string.IsNullOrEmpty(txtImpuesto.Text) || string.IsNullOrEmpty(txtPrecio.Text))
+            if (string.IsNullOrEmpty(txtCodMedicamento.Text) || string.IsNullOrEmpty(txtDescuento.Text) || string.IsNullOrEmpty(txtImpuesto.Text) || string.IsNullOrEmpty(txtPrecio.Text))
             {
                 //Datos vacios
                 MessageBox.Show("Hay datos vacios");
@@ -46,13 +46,13 @@ namespace SistemaFarmacia
                 //SE MODIFICA SEGUN DATOS DE LA BASE DE DATOS Y [DISEÑO]
                 string QryAgregarDetalleVenta = "Insert into tbl_detalle_ventas (CodigoVenta, CodigoMedicamento, Cantidad, PrecioUnitario, Descuento, Impuesto, Total) values (@CodigoVenta, @CodigoMedicamento, @Cantidad, @PrecioUnitario, @Descuento, @Impuesto, @Total)";
                 SqlCommand cmd = new SqlCommand(QryAgregarDetalleVenta, conn);
-                cmd.Parameters.AddWithValue("@CodigoVenta", txtCodVenta.Text);
-                cmd.Parameters.AddWithValue("@CodigoMedicamento", txtCodMedicamento.Text);
-                cmd.Parameters.AddWithValue("@Cantidad", txtCantidad.Text);
-                cmd.Parameters.AddWithValue("@PrecioUnitario", txtPrecio.Text);
-                cmd.Parameters.AddWithValue("@Descuento", txtDescuento.Text);
-                cmd.Parameters.AddWithValue("@Impuesto", txtImpuesto.Text);
-                cmd.Parameters.AddWithValue("@Total", txtTotal.Text);
+                cmd.Parameters.AddWithValue("@CodigoVenta", int.Parse(txtCodVenta.Text));
+                cmd.Parameters.AddWithValue("@CodigoMedicamento", int.Parse(txtCodMedicamento.Text));
+                cmd.Parameters.AddWithValue("@Cantidad", int.Parse(txtCantidad.Text));
+                cmd.Parameters.AddWithValue("@PrecioUnitario", decimal.Parse(txtPrecio.Text));
+                cmd.Parameters.AddWithValue("@Descuento", decimal.Parse(txtDescuento.Text));
+                cmd.Parameters.AddWithValue("@Impuesto", decimal.Parse(txtImpuesto.Text));
+                cmd.Parameters.AddWithValue("@Total", decimal.Parse(txtTotal.Text));
 
                 // VERIFICA SI SE AGREGARON FILAS
                 cant = cmd.ExecuteNonQuery();
@@ -109,14 +109,14 @@ namespace SistemaFarmacia
                 // Captura datos formulario y actualiza en base de datos
                 string QryActualizaDetalleVenta = "Update tbl_detalle_ventas set CodigoVenta=@CodigoVenta, CodigoMedicamento=@CodigoMedicamento, Cantidad=@Cantidad, PrecioUnitario=@PrecioUnitario, Descuento=@Descuento, Total=@Total where CodigoDetalleVenta=@CodigoDetalleVenta";
                 SqlCommand commandQryActualiza = new SqlCommand(QryActualizaDetalleVenta, conn);
-                commandQryActualiza.Parameters.AddWithValue("@CodigoDetalleVenta", txtCodigoDetalleV.Text);
-                commandQryActualiza.Parameters.AddWithValue("@CodigoVenta", txtCodVenta.Text);
-                commandQryActualiza.Parameters.AddWithValue("@CodigoMedicamento", txtCodMedicamento.Text);
-                commandQryActualiza.Parameters.AddWithValue("@Cantidad", txtCantidad.Text);
-                commandQryActualiza.Parameters.AddWithValue("@PrecioUnitario", txtPrecio.Text);
-                commandQryActualiza.Parameters.AddWithValue("@Descuento", txtDescuento.Text);
-                commandQryActualiza.Parameters.AddWithValue("@Impuesto", txtImpuesto.Text);
-                commandQryActualiza.Parameters.AddWithValue("@Total", txtTotal.Text);
+                commandQryActualiza.Parameters.AddWithValue("@CodigoDetalleVenta", int.Parse(txtCodigoDetalleV.Text));
+                commandQryActualiza.Parameters.AddWithValue("@CodigoVenta", int.Parse(txtCodVenta.Text));
+                commandQryActualiza.Parameters.AddWithValue("@CodigoMedicamento", int.Parse(txtCodMedicamento.Text));
+                commandQryActualiza.Parameters.AddWithValue("@Cantidad", int.Parse(txtCantidad.Text));
+                commandQryActualiza.Parameters.AddWithValue("@PrecioUnitario", decimal.Parse(txtPrecio.Text));
+                commandQryActualiza.Parameters.AddWithValue("@Descuento", decimal.Parse(txtDescuento.Text));
+                commandQryActualiza.Parameters.AddWithValue("@Impuesto", decimal.Parse(txtImpuesto.Text));
+                commandQryActualiza.Parameters.AddWithValue("@Total", decimal.Parse(txtTotal.Text));
                 cant = commandQryActualiza.ExecuteNonQuery();
 
                 // Valida si se actualizaron datos en la base de datos
@@ -161,6 +161,46 @@ namespace SistemaFarmacia
             txtTotal.Text = dgvDetalleV.SelectedCells[7].Value.ToString();
             //ESTO SIRVE PARA CAPTURAR LOS DATOS DE LA BASE DE DATOS Y MANDARLOS AL FORM EDITABLE
 
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            int cant = 0;
+            conn.Open();
+
+            // Captura datos formulario y elimina en base de datos
+            string QryDetalleVentas = "Delete from tbl_detalle_ventas  where CodigoDetalleVenta=@CodigoDetalleVenta";
+            SqlCommand commandQryEliminar = new SqlCommand(QryDetalleVentas, conn);
+            commandQryEliminar.Parameters.AddWithValue("@CodigoDetalleVenta", txtCodigoDetalleV.Text);
+            cant = commandQryEliminar.ExecuteNonQuery();
+
+            // Valida si se eliminaron datos en la base de datos
+            if (cant > 0)
+            {
+                MessageBox.Show("Registro Eliminado!!!", "Confirmación", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                txtCodigoDetalleV.Text = "";
+                txtCantidad.Text = "";
+                txtBusqueda.Text = "";
+                txtCodMedicamento.Text = "";
+                txtCodVenta.Text = "";
+                txtDescuento.Text = "";
+                txtPrecio.Text = "";
+                txtTotal.Text = "";
+                txtImpuesto.Text = "";
+
+                string QryConsultarDetalleVenta = "Select * from tbl_detalle_ventas";
+                SqlDataAdapter adapter = new SqlDataAdapter(QryConsultarDetalleVenta, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dgvDetalleV.DataSource = dt;
+
+            }
+            else
+            {
+                MessageBox.Show("No se elimino el registro!!!", "Advertencia!!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            conn.Close();
         }
     }
 }
